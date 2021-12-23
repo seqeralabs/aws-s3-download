8b4e5ced8076dbdd29a512709afb80a3855e551d
Jordi Deu-Pons <jordi@jordeu.net>
2021-12-23 08:51:48 +0100

    Set max_connections

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -1,6 +1,10 @@
+import java.util.Map;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.ClientConfigurationFactory;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.transfer.Download;
@@ -39,7 +43,10 @@ public class Main1 {
             System.out.println("Using AWS region eu-west-1. Use variable AWS_DEFAULT_REGION to change it");
         }
 
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(region).build();
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withClientConfiguration(buildClientConfiguration())
+                .withRegion(region)
+                .build();
 
         java.io.File f = new java.io.File(args[1]);
         TransferManager xfer_mgr = TransferManagerBuilder.standard().withS3Client(s3).build();
@@ -60,6 +67,25 @@ public class Main1 {
         xfer_mgr.shutdownNow();
 
 
+    }
+
+    private static ClientConfiguration buildClientConfiguration() {
+        final Map<String, String> env = System.getenv();
+        final Properties props = System.getProperties();
+        final ClientConfiguration config = new ClientConfiguration();
+
+        if (env.containsKey("AWS_MAX_CONNECTIONS") || props.containsKey("aws.max_connections")) {
+            String maxConnections = env.getOrDefault("AWS_MAX_CONNECTIONS", props.getProperty("aws.max_connections"));
+            try {
+                config.setMaxConnections(Integer.parseInt(maxConnections));
+            }
+            catch (NumberFormatException e) {
+                System.err.println(String.format("Invalid number format max_connections value: %s", maxConnections));
+                System.exit(1);
+            }
+        }
+
+        return config;
     }
 
 
